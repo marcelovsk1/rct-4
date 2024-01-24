6df0d4877439fc9caba227a55aea246840153742
Marcelo Amaral Alves <marceloaa@live.com>
2024-01-24 10:58:33 -0300

    working on

--- orignals-mkt/src/components/Layout.jsx
+++ orignals-mkt/src/components/Layout.jsx
@@ -4,11 +4,13 @@ import Routers from '../../routes/Routers'
 import Header from './Header/header'
 import Footer from './Footer/footer'
 
-const Layout = (props) => {
+const Layout = () => {
   return(
     <>
       <Header/>
-      <div>{props.children}</div>
+      <div>
+        <Routers/>
+      </div>
       <Footer/>
     </>
   )

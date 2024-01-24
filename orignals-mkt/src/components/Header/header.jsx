94f19c4492ad6f742b6b89ec0e9ba1a2ed14ef32
Marcelo Amaral Alves <marceloaa@live.com>
2024-01-24 16:04:24 -0300

    working on

--- orignals-mkt/src/components/Header/header.jsx
+++ orignals-mkt/src/components/Header/header.jsx
@@ -1,9 +1,24 @@
 import React from "react";
+import "./header.scss"
+import { Container } from "reactstrap";
 
 const Header = () => {
-  return <div>
-      Header
-    </div>
+  return (
+  <header className="header">
+    <Container>
+      <div className="navigation">
+        <div className="logo">
+          <h2 className=" d-flex gap-2 align-items-center ">
+            <span>
+              <i className="ri-fire-fill"></i>
+            </span>
+            Originals
+          </h2>
+        </div>
+      </div>
+    </Container>
+  </header>
+  )
 };
 
 export default Header;

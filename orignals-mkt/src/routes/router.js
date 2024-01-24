f35275546d0a19e6179307123e2735239aff89d0
Marcelo Amaral Alves <marceloaa@live.com>
2024-01-24 10:42:23 -0300

    working on

--- orignals-mkt/src/routes/router.js
+++ orignals-mkt/src/routes/router.js
@@ -1,11 +1,14 @@
 
 import React from 'react'
+
+import { Routes, Route, Navigate } from 'react-router-dom'
+
 import Home from '../pages/home'
 import Market from '../pages/market'
 import Gazelle from '../pages/gazelle'
 import Spezial from '../pages/spezial'
-import Samba from '../pages/samba'
-import Campus from '../pages/samba'
+import Samba from '../pages/campus'
+import Campus from '../pages/campus'
 import Wallet from '../pages/wallet'
 
 const Routers = () => {

d206d5413bc8de8f9094c4eef5aa8f563bbdaf55
Marcelo Amaral Alves <marceloaa@live.com>
2024-01-24 11:06:01 -0300

    working on

--- orignals-mkt/src/components/Layout/Layout.jsx
+++ orignals-mkt/src/components/Layout/Layout.jsx
@@ -1,6 +1,6 @@
 
 import React from 'react'
-import Routers from '../../routes/Routers'
+import Routers from '../../routes/router'
 import Header from '../Header/header'
 import Footer from '../Footer/footer'
 
